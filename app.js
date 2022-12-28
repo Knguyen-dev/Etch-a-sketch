7316f719bab5dd7447304f5ff43adc0f24fd987e
Kevin Nguyen <thenoc27@gmail.com>
2022-12-28 13:25:38 -0500

    Basic page structuring before designing the grid

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+- Create divs using javascript 
+- Create a title 
+- Create a side panel, with color selectors, so if you click on color mode, then you pick a color with a circular color selector. 
+- If you pick rainbow mode the colors will show rainbow colors
+- Have a gradient selector, kind of like every 10 pixels, it makes the color either lighter or darker, and it gradually does this for every 5 or 10 grids
+- If you pick eraser then it turns the grid pixels white
+- If you pick clear the nit clears the board of colors, making everything white
+- There's also a slider at the bottom where you should be able to choose how many grids you want
+- Then have a copyright at the bottom 
+
++ Challenge after everything is done, try to make header an interactive grid that the user can hover over and see the different squares and colors
+*/
+
+
+
