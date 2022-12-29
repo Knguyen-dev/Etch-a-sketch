abb764f870c1123e53b44d67224008e8f109fb31
Kevin Nguyen <thenoc27@gmail.com>
2022-12-29 13:19:06 -0500

    Split the grid lines functionality into two functions and amde it so the grid style would persist when generating new grid formats. Obtained the select color, rainbow, eraser, and gradient buttons and made htem into a node list, rather than having an individual variable for each of them. Added event listener for each of those buttons to indicate their selection visually and in the javascript to set up for a function that changes the grid pixels based on the selection. Most importantly the logic works now that when you click down and hover over the grid pixels it colors them and if you mouse up then it doesn't color them

--- app.js
+++ app.js
@@ -23,18 +23,48 @@ const sliderValueEl = document.querySelector('.slider-value');
 const sliderEl = document.querySelector('.slider');
 const gridEl = document.querySelector('.color-grid');
 const colorPickerEl = document.getElementById('colorPicker');
-const colorBtn = document.getElementById('select-color-btn');
+const colorBtns = document.querySelectorAll('.color-btn');
 const toggleGridBtn = document.getElementById('toggle-grid');
 const clearGridBtn = document.getElementById('clear-btn');
 
 
 let selectedColor;
+let selectedButton = "";
+// Now with these buttons we will select the four (color, rainbow, gradient, and eraser)
+// For the first time. Then we will need make them listen to events, once a button is clicked
+// we visually select it and visually deselect other buttons. As well as this we store
+// the id of the selected button into a variable. Only one option can be selected at a time
+// So keep reassigning when clicked. 
+
+colorBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const btnID = e.currentTarget.dataset.id;
+    //if button is newly selected then visually and logically add
+    if (!btn.classList.contains('color-btn-selected')) {
+      selectedButton = btnID;
+      btn.classList.add('color-btn-selected'); //visually select button
+      colorBtns.forEach(item => {
+        if (item.dataset.id !== btnID) { //for the different buttons (differing id) delsect them
+          item.classList.remove('color-btn-selected');
+        }
+      })
+    } else {
+      selectedButton = ""; //clear the selectedButton variable since the user hasn't selected anything
+      btn.classList.remove('color-btn-selected'); //visually deselect the button that the user is clicking on since they want to deselect it
+    }
+  })
+});
 
 //Gets hex color and displays it on the color button.
 //Stores the color into selectedColors for further usage.
-function display_color_value() {
+function set_color_value() {
   const hexColor = colorPickerEl.value;
-  colorBtn.textContent = hexColor;
+  // Find the select Color button in the button list
+  colorBtns.forEach(btn => {
+    if (btn.dataset.id == "select-color") {
+      btn.textContent = hexColor;
+    }
+  })
   selectedColor = hexColor;
 }
 
@@ -42,24 +72,32 @@ function displaySliderValue(sliderValue) {
   sliderValueEl.textContent = `${sliderValue} x ${sliderValue}`;
 }
 
+// Two to persist the grid lines, one is just getting the grid items after and calling a function
+// The other is directly manipulating the html in the for loop
+// The third, which is interesting, would be creating a function for making the html
+// board and having an entire function dedicated to rendering it on screen, which isn't a bad idea
 function displayGrid() {
   // Get the slider value from the slider
   const sliderValue = sliderEl.value;
   // Calculate the amount of pixels or sections that'll be on the grid
   const amount = sliderValue * sliderValue;
   let gridItemsHTML = []; //Stores the html for the grid items
-
   // Style the grid so that it fits the sections; both may not be necessary
   gridEl.style.gridTemplateColumns = `repeat(${sliderValue}, 1fr)`;
   gridEl.style.gridTemplateRows = `repeat(${sliderValue}, 1fr)`;
-
   // Create the grid items and put them into an array
   for (let x = 0; x < amount; x++) {    
     gridItemsHTML.push(`<div class="grid-item"></div>`);
   }
   // Reset the html of the grid to the new items of the grid
   gridEl.innerHTML = gridItemsHTML.join("");
-
+  
+  // Persists the style of the grid in this newly generated grid
+  if (toggleGridBtn.dataset.state == "on") {
+    displayGridLines();
+  } else {
+    removeGridLines();
+  }
   // Display the new dimension of the grid on the slider section
   displaySliderValue(sliderValue);
 }
@@ -71,33 +109,68 @@ function clearGrid() {
   });
 }
 
+// Let the grid style persist even when rendering new grid
+// When rendering new grid we want to call a function that turns off the grid
+// Then for the toggleGridBtn we should probably make two functions and based on the 
+// data type probably we select whether its the turn on function or the turn off function
+function removeGridLines() {
+  const gridItems = gridEl.querySelectorAll('.grid-item');
+  gridItems.forEach(function(item) {
+    item.style.border = 'none';
+  })
+}
+function displayGridLines() {
+  const gridItems = gridEl.querySelectorAll('.grid-item');
+  gridItems.forEach(function(item) {
+    item.style.border = '1px solid black';
+  })
+}
+/*
+- Function that handles filling in the colors for each individual grid pixel
+- The event being passed is the div or pixel in the grid that 
+- We should
+*/
+function changeGridPixel(e) {
+  const currentPixel = e.currentTarget; 
+  currentPixel.style.background = selectedColor;
+}
+
 sliderEl.addEventListener("input", displayGrid);
-colorPickerEl.addEventListener("input", display_color_value);
+colorPickerEl.addEventListener("input", set_color_value);
 clearGridBtn.addEventListener('click', clearGrid);
+
+
 
 toggleGridBtn.addEventListener('click', function(e) {
   const gridState = e.currentTarget.dataset.state;  
   const gridItems = gridEl.querySelectorAll('.grid-item');
-
   if (gridState == 'on') {
     toggleGridBtn.classList.add('toggle-btn-selected');
-    // Turn off grid lines and set button's state attribute to off so that the next time the user selects it, it turns the lines back on
-    gridItems.forEach(function(item) {
-      item.style.border = 'none';
-    });
+    removeGridLines();
     toggleGridBtn.setAttribute('data-state', 'off'); 
   } else {
     toggleGridBtn.classList.remove('toggle-btn-selected');
-    gridItems.forEach(function(item) {
-      item.style.border = '1px solid black';
-    });
+    displayGridLines();
     toggleGridBtn.setAttribute('data-state', 'on');
   }
 });
 
+function toggleGridEventListener(e) {
+  const gridItems = gridEl.querySelectorAll('.grid-item');
+  if (e.type == "mousedown") {
+    gridItems.forEach(function(item) {
+      item.addEventListener('mouseover', changeGridPixel);
+    })
+  } else if (e.type == "mouseup") {
+    gridItems.forEach(function(item){
+      item.removeEventListener('mouseover', changeGridPixel);
+    })
+  }
+}
 
-
+window.addEventListener('mousedown', toggleGridEventListener);
+window.addEventListener('mouseup', toggleGridEventListener);
 window.addEventListener('DOMContentLoaded', function() {
   displayGrid();
-  display_color_value();
+  set_color_value();
 });
